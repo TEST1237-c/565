9c0bee8305a6828bf38f3a3c4e271778634e6606
TEST1237-c <807shopoff3@gmail.com>
2026-05-25 18:48:14 +0200

    Update maintenance-check.js

--- maintenance-check.js
+++ maintenance-check.js
@@ -1,19 +1,16 @@
 (function () {
     'use strict';
 
-    var EXEMPT_PAGES = ['maintenance.html', 'admin.html'];
     var path = window.location.pathname;
-    var currentPage = path.split('/').pop() || 'index.html';
 
-    for (var i = 0; i < EXEMPT_PAGES.length; i++) {
-        if (path.indexOf(EXEMPT_PAGES[i]) !== -1) return;
-    }
+    // Exempter maintenance et admin (avec ou sans .html)
+    if (path.indexOf('maintenance') !== -1 || path.indexOf('admin') !== -1) return;
 
     fetch('/api/maintenance-status', { cache: 'no-store' })
         .then(function (res) { return res.json(); })
         .then(function (data) {
             if (data.enabled) {
-                window.location.href = '/maintenance.html';
+                window.location.href = '/maintenance';
             }
         })
         .catch(function () {});
